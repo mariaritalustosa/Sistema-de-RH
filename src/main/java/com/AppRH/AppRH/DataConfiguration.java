42ef061e1e8825ecf2a7c874e20b317c7af37868
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-07-24 12:41:15 -0300

    inicia configuração do DataSource com @Bean

--- src/main/java/com/AppRH/AppRH/DataConfiguration.java
+++ src/main/java/com/AppRH/AppRH/DataConfiguration.java
@@ -1,11 +1,13 @@
 package com.AppRH.AppRH;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataConfiguration {
 
+    @Bean
     public DataSource dataSource(){
-        
+
     }
 }
