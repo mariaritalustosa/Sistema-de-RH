2323a4c5eb2fb7568dbd3411cb8385085e928895
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-07-24 12:45:43 -0300

    configuração do banco de dados

--- src/main/java/com/AppRH/AppRH/DataConfiguration.java
+++ src/main/java/com/AppRH/AppRH/DataConfiguration.java
@@ -1,13 +1,17 @@
 package com.AppRH.AppRH;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DataConfiguration {
 
     @Bean
     public DataSource dataSource(){
-
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
     }
 }
