d1e27113ca861ec778d2012ea760e099729c3bdf
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-07-24 12:38:50 -0300

    first commit

---
+++ src/main/java/com/AppRH/AppRH/DataConfiguration.java
@@ -0,0 +1,11 @@
+package com.AppRH.AppRH;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataConfiguration {
+
+    public DataSource dataSource(){
+        
+    }
+}
